20a8dbb024a90899525c675023aae9afc51106d3
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-29 16:59:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // write js code here if required
+const date = new Date();
+let day = date.getDay();
+let month = date.getMonth();
+let year = date.getFullYear();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+// alert ( year)
+document.getElementById("timer").innerText = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`
